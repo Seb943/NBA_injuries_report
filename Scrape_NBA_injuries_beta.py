2ef097e055e4e5fa4c1a8f4bb01c09246ffc24a0
Seb943 <55142014+Seb943@users.noreply.github.com>
2020-12-24 18:42:06 +0100

    Add files via upload

---
+++ Scrape_NBA_injuries_beta.py
@@ -0,0 +1,44 @@
+# Scrape NBA injuries news and send the recapitulative table by mail
+import os
+from selenium import webdriver
+import pandas as pd
+from datetime import date
+
+global driver
+driver = webdriver.Chrome(executable_path = "C:\\Users\\Utilisateur\\Desktop\\scrapeOP-master\\chromedriver_win32\\chromedriver.exe")
+driver.get('https://www.rotoworld.com/basketball/nba/injury-report')
+
+########################### I - First define the functions
+def ffi(a):
+    try:
+        return(driver.find_element_by_xpath(a).text)
+    except:
+        return(None)
+
+def collect_info_line(team, i):
+    TEAM = ffi('//*[@id="injury-report-page-wrapper"]/div/div[2]/div[{}]/a/div/div[2]'.format(team))
+    player = ffi('//*[@id="injury-report-page-wrapper"]/div/div[2]/div[{}]/div/div/div[1]/table/tbody/tr[{}]/td[1]/span/a'.format(team, i))
+    position = ffi('//*[@id="injury-report-page-wrapper"]/div/div[2]/div[{}]/div/div/div[1]/table/tbody/tr[{}]/td[2]'.format(team, i))
+    status = ffi('//*[@id="injury-report-page-wrapper"]/div/div[2]/div[{}]/div/div/div[1]/table/tbody/tr[{}]/td[3]'.format(team, i))
+    date = ffi('//*[@id="injury-report-page-wrapper"]/div/div[2]/div[{}]/div/div/div[1]/table/tbody/tr[{}]/td[4]'.format(team, i))
+    injury = ffi('//*[@id="injury-report-page-wrapper"]/div/div[2]/div[{}]/div/div/div[1]/table/tbody/tr[{}]/td[5]'.format(team, i))
+    returns = ffi('//*[@id="injury-report-page-wrapper"]/div/div[2]/div[{}]/div/div/div[1]/table/tbody/tr[{}]/td[6]'.format(team, i))
+
+    if player is not None:
+        print(TEAM, player, status)
+    return([TEAM, player, position, status, date, injury, returns])
+
+########################## II - Now scrape the page...
+Injuries = []
+for team in range(1, 31):
+    for i in range(1, 50):
+        info = collect_info_line(team, i)
+        if info[1] is not None:
+            Injuries.append(info)
+        else:
+            break
+
+########################## III - Save the csv
+Injuries = pd.DataFrame(Injuries)
+Injuries.columns = ['Team', 'Player', 'Position', 'Status', 'News date', 'Injury', 'Return date']
+Injuries.to_csv('Injuries_report_{}.csv'.format(date.today()), index = False, sep = ';')
